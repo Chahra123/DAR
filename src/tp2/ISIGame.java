5b3d156a2b3082c722ae65f27ca3975196324806
Chahrazed Benazaiez <61512926+Chahra123@users.noreply.github.com>
2021-11-11 16:48:09 +0100

    3CS1_TP2_ChahrazedBenAzaiez_ISIGAME.java

--- src/tp2/ISIGame.java
+++ src/tp2/ISIGame.java
@@ -1,24 +1,27 @@
-package tp2;
+package tp2_isigame;
 
+import java.util.Locale;
 import java.util.Scanner;
 
-public class ISIGame {
-
+public class game {
     static class ISIException extends RuntimeException{
         public ISIException(String s) {
+
             super(s);
         }
     }
 
     public static void main(String[] args) {
         System.out.println("Enter a word");
-
-        Scanner scanner = new Scanner(System.in);
-        String line = scanner.nextLine();
-
-        // now you have to check if the word contains 'isi' or not
-        // if not print 'ok'
-        // if yes throw the exception 'ISIException'
-        // and when you catch it print 'life is not isi'
+        try {
+            Scanner scanner = new Scanner(System.in);
+            String line = scanner.nextLine();
+            if (!line.toLowerCase().contains("isi")) {
+                System.out.println("ok");
+            }
+            else {throw new ISIException(line);}
+        } catch(Exception e){
+            System.out.println("life is not easy");
     }
-}
+}}
+
