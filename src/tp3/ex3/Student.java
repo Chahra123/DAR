bf30d5a59dc83de16ae3d21b8d0c9ad070c4c847
Chahrazed Benazaiez <61512926+Chahra123@users.noreply.github.com>
2021-12-04 09:59:05 +0100

    Update Student.java

--- src/tp3/ex3/Student.java
+++ src/tp3/ex3/Student.java
@@ -1,4 +1,5 @@
-package tp3.ex3;
+package ex3;
+
 
 public class Student extends Thread{
     private ISISandwich isiSandwich;
@@ -7,11 +8,18 @@ public class Student extends Thread{
         super(s);
         this.isiSandwich = isiSandwich;
     }
-
     @Override
-    public void run() {
+    public void run() {synchronized(isiSandwich) {
         System.out.println("Student "+this.getName()+" is waiting for a sandwich");
+        try {
+			isiSandwich.wait();
+			System.out.println("Student "+this.getName()+" got a sandwich");
+		} catch (InterruptedException e) {
+			
+			e.printStackTrace();
+		}
         //todo : wait for a sandwich until it's prepared
-        System.out.println("Student "+this.getName()+" got a sandwich");
+        
     }
+}
 }
