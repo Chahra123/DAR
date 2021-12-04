379d6be579d1f767e4235dbd1547e7ecb4a997a4
Chahrazed Benazaiez <61512926+Chahra123@users.noreply.github.com>
2021-12-04 09:58:40 +0100

    Update Main.java

--- src/tp3/ex3/Main.java
+++ src/tp3/ex3/Main.java
@@ -1,22 +1,25 @@
-package tp3.ex3;
+package ex3;
 
-public class Main {
+public class MAIN {
 
-    // in this assignment we will simulate a vendor that sells an isi sandwich
-    // and a student that waits for a sandwich until it's prepared
-    // implement the student and the vendor run methods in order to get a correct output
 
-    public static void main(String[] args) {
-        ISISandwich isiSandwich = new ISISandwich();
-        Student student = new Student("Salah",isiSandwich);
-        Vendor vendor = new Vendor("El Baya3",isiSandwich);
+	    // in this assignment we will simulate a vendor that sells an isi sandwich
+	    // and a student that waits for a sandwich until it's prepared
+	    // implement the student and the vendor run methods in order to get a correct output
 
-        student.start();
-        vendor.start();
+	    public static void main(String[] args) {
+	        ISISandwich isiSandwich = new ISISandwich();
+	        Student student = new Student("Salah",isiSandwich);
+	        Vendor vendor = new Vendor("El Baya3",isiSandwich);
 
-        //output should be :
-        //  Student Salah is waiting for a sandwich
-        //  Vendor El Baya3 said 'isi sandwich is ready'
-        //  Student Salah got a sandwich
-    }
-}
+	        student.start();
+	        vendor.start();
+
+	        //output should be :
+	        //  Student Salah is waiting for a sandwich
+	        //  Vendor El Baya3 said 'isi sandwich is ready'
+	        //  Student Salah got a sandwich
+	    }
+	}
+
+
